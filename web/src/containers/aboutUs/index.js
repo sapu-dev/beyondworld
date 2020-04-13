baec6d9875a9e2a73a9e4a69d7c3b1e48decdcba
Sapumal Thepulangoda <sapumal.thepulangoda@auxenta.com>
2020-04-13 12:01:02 +0530

    remove all the content

--- web/src/containers/aboutUs/index.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from "react";
-
-class AboutUs extends Component {
-  render() {
-    return <div>about us</div>;
-  }
-}
-
-export default AboutUs;
--- web/src/containers/oders/index.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from "react";
-
-class Orders extends Component {
-  render() {
-    return <div>Orders</div>;
-  }
-}
-
-export default Orders;
